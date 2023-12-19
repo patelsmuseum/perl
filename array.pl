c205a60f003a964a3a180b425b5198c72da8b28b
Purshotam <pankajpurshotam@gmail.com>
2023-12-19 15:36:31 +0530

    array

--- array.pl
+++ array.pl
@@ -16,5 +16,31 @@ print("\n");
 
 my $number =scalar  @fruits;
 
+my $count = $#fruits;
+
+print($count);
+
+print("\n");
+
 print($number);
 
+print("\n");
+
+print(@fruits);
+
+@fruits[1,3] = 'grapes' , 'tree' , 'banana';
+
+print("\n");
+
+print(@fruits);
+
+
+print("\n");
+
+# ************************ stack *************************
+my @stack = ();
+push(@stack , 1);
+push(@stack , 4);
+push(@stack , 7);
+
+print("@stack");
